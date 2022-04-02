199fe2584c3cb34e7625cd553819266da999a38e
aidarjpg <aidar0amangeldi@gmail.com>
2022-04-02 18:32:08 +0600

    -coefficent added

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ fig, ax = plt.subplots()
 x, y = [0.0, 3.0, 6.0, 4.0],[0.0, 3*math.sqrt(3), 0.0, 3.0]
 sc = ax.scatter(x,y, s=1)
 i = 3
-def plotmid(i):
-    while(i<=100):
-        value = randint(0, 2)#choose random point 
-        x.append((x[value] + x[i])/2.0) #find midX
-        y.append((y[value] + y[i])/2.0) #find midY
+def plotmid(i, coef):
+    while(i<=10000):
+        value = randint(0, 2) #choose random point 
+        x.append((x[value] + x[i])*coef) #find midX
+        y.append((y[value] + y[i])*coef) #find midY
         sc.set_offsets(np.c_[x,y]) #plot midPoint
         i = i + 1
 
 def main():
-    ani = FuncAnimation(plt.gcf(), plotmid, interval = 10, repeat=True)
+    ani = FuncAnimation(plt.gcf(), plotmid(i, 0.5), interval = 10, repeat=True)
     plt.show()
     
 if __name__ == "__main__":
